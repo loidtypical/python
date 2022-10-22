ea9d406cf5e62cdbc13095b8222fbbcadf3dd87f
unknown <opanasenkovolodar@gmail.com>
2022-10-22 12:36:49 +0300

    занятие 3

---
+++ занятие 3/main.py
@@ -0,0 +1,15 @@
+# OMG=int(input())
+# print(OMG)
+# print(OMG+1)
+# print(OMG+2)
+
+# ke=int(input())
+# kb=int(input())
+# kr=int(input())
+# print(ke+kb+kr)
+
+r=int(input())
+v=r*r*r
+s=6*r*r
+print("Объем =",v)
+print("Площадь полной поверхности =",s)
