483886a662c537f9266abeee772f9dec65ecfbd6
unknown <opanasenkovolodar@gmail.com>
2022-11-04 09:35:01 +0200

    7

---
+++ занятие 7/main.py
@@ -0,0 +1,24 @@
+# print("*"*17)
+# print("*               *")
+# print("*               *")
+# print("*"*17)
+
+# a=int(input())
+# b=int(input())
+# v=(a+b)**2
+# g=(a**2)+(b**2)
+# print("Квадрат суммы",a,"и",b,"равен",v)
+# print("Сумма квадратов",a,"и",b,"равна",g)
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=int(input())
+# f=(a**b)
+# h=(c**d)
+# print(f+h)
+
+n=int(input())
+nn=n*10+n
+nnn=n*100+nn
+print(n+nn+nnn)
