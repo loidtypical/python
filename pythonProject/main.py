bc4e1acb48d93d247a9eddd878a99c76e36897b4
unknown <opanasenkovolodar@gmail.com>
2022-11-09 16:57:58 +0200

    8

---
+++ pythonProject/main.py
@@ -0,0 +1,61 @@
+# a=input()
+# b=input()
+# if a == b:
+#     print("Пароль принят")
+# else:
+#     print("Пароль не принят")
+
+# c=int(input())
+# if c % 2 == 0:
+#     print("Четное")
+# else:
+#     print("Нечетное")
+
+# f=int(input())
+# o=f//1000
+# i=(f//100)%10
+# k=(f//10)%10
+# n=f%10
+# if o+n==i-k:
+#     print("ДА")
+# else:
+#     print("НЕТ")
+
+
+# p=int(input())
+# if 18<=p:
+#     print("Доступ разрешен")
+# else:
+#     print("Доступ запрещен")
+
+# o=int(input())
+# p=int(input())
+# if o<p:
+#     print(o)
+# else:
+#     print(p)
+
+# u=int(input())
+# if u<=13:
+#     print("детство")
+# if 14<=u <=24:
+#     print("молодость")
+# if 25<=u <=59:
+#     print("зрелость")
+# if u>=60:
+#     print("старость")
+
+r=int(input())
+b=int(input())
+v=int(input())
+g=0
+if r>0:
+    g=r
+if b>0:
+    g=g+b
+if v>0:
+    g=g+v
+print(g)
+
+
+
